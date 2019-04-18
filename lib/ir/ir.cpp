108316f1f711acf2336caf6b8395c3a508553e20
Daniel Shchur <shchurgood@gmail.com>
2019-04-18 15:32:39 -0500

    Removed Serial, moved constants to header
    Removed serial debug statements, moved all constant defines to the header.

--- lib/ir/ir.cpp
+++ lib/ir/ir.cpp
@@ -30,27 +30,7 @@
 #include <Arduino.h>
 #include <avr/io.h>
 #include "motors.h"
-
-#define SAMPLE_SIZE       34
-#define IR_DECODER_PIN    (PIND & (1 << PIND2))
-#define IR_PIN            PD2
-#define SET_FALLING       (EICRA = (EICRA & ~(1 << ISC00)) | (1 << ISC01))
-#define SET_RISING        (EICRA |= (1 << ISC00) | (1 << ISC01))
-// falling if 0, rising if 1
-#define EDGE              (EICRA & (1 << ISC00))
-#define TRUE              1
-#define FALSE             0
-
-// IR Data Constants
-#define POWER_BTN   0xFFA25D
-#define FUNC_BTN    0xFFE21D
-#define VOLUP_BTN   0xFF629D
-#define VOLDOWN_BTN 0xFFA857
-#define LSKP_BTN    0xFF22DD
-#define RSKP_BTN    0xFFC23D
-#define PAUSE_BTN   0xFF02FD
-#define DOWN_BTN    0xFFE01F
-#define UP_BTN      0xFF906F
+#include "ir.h"
 
 // Set built in reset function to PC 0
 void(* resetFunc) (void) = 0;
@@ -66,7 +46,6 @@ volatile uint8_t dataReady;
  */
 void decodeIR() {
   // Waiting for new IR packet
-  Serial.println("Waiting for new Packet");
   while(!newIrPacket);
   newIrPacket = FALSE;
 
@@ -112,9 +91,6 @@ void decodeIR() {
 void setUpIR() {
   // Turn off WDT 
   WDTCSR = 0x00; 
-  
-  Serial.begin(9600);
-  Serial.println("Starting up.");
 
   // Reset settings
   TCCR1A = 0x0;
--- lib/ir/ir.h
+++ lib/ir/ir.h
@@ -1,3 +1,24 @@
+#define SAMPLE_SIZE       34
+#define IR_DECODER_PIN    (PIND & (1 << PIND2))
+#define IR_PIN            PD2
+#define SET_FALLING       (EICRA = (EICRA & ~(1 << ISC00)) | (1 << ISC01))
+#define SET_RISING        (EICRA |= (1 << ISC00) | (1 << ISC01))
+// falling if 0, rising if 1
+#define EDGE              (EICRA & (1 << ISC00))
+#define TRUE              1
+#define FALSE             0
+
+// IR Data Constants
+#define POWER_BTN   0xFFA25D
+#define FUNC_BTN    0xFFE21D
+#define VOLUP_BTN   0xFF629D
+#define VOLDOWN_BTN 0xFFA857
+#define LSKP_BTN    0xFF22DD
+#define RSKP_BTN    0xFFC23D
+#define PAUSE_BTN   0xFF02FD
+#define DOWN_BTN    0xFFE01F
+#define UP_BTN      0xFF906F
+
 /**
  * Sets up the IR
 */ 
